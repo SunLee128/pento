79593c200f75b3217271a6dfc0fc44eb1b3ebfd6
SunLee128 <sunlee128@gmail.com>
2022-05-23 18:10:45 +1000

    Add authentication layer

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -8,12 +8,22 @@ defmodule PentoWeb.WrongLive do
   def render(assigns) do
     ~H"""
     <h1> your score: <%= @score %> </h1>
-    <h2><%= @message %></h2>
+    <h2><%= @message %>It's <%= time()%></h2>
     <h2>
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
     </h2>
     """
+  end
+
+  def time do
+    DateTime.utc_now |> to_string
+  end
+
+  def handle_event("guess", %{ "number" => guess}=data, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again."
+    score = socket.assigns.score - 1
+    {:noreply, assign(socket, score: score, message: message)}
   end
 end
