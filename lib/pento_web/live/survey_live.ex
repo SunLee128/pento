fc2d3cbaa9d9234629e5dbc89461da1320740d27
SunLee128 <sunlee128@gmail.com>
2022-06-22 20:26:56 +1000

    Implemented live dashboard

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -1,7 +1,9 @@
 defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias Pento.{Survey, Catalog}
-  alias PentoWeb.{DemographicLive, RatingLive}
+  alias PentoWeb.{DemographicLive, RatingLive, Endpoint}
+
+  @survey_results_topic "survey_results"
 
   def mount(_params, _session, socket) do
     IO.inspect(socket.assigns.current_user)
@@ -26,16 +28,6 @@ defmodule PentoWeb.SurveyLive do
     |> assign(:demographic, demographic)
   end
 
-  def handle_rating_created(
-        %{assigns: %{products: products}} = socket,
-        updated_product,
-        product_index
-      ) do
-    socket
-    |> put_flash(:info, "Rating submitted succesfully")
-    |> assign(:products, List.replace_at(products, product_index, updated_product))
-  end
-
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(
       socket,
@@ -50,5 +42,20 @@ defmodule PentoWeb.SurveyLive do
 
   defp list_products(user) do
     Catalog.list_products_with_user_ratings(user)
+  end
+
+  defp handle_rating_created(
+         %{assigns: %{products: products}} = socket,
+         updated_product,
+         product_index
+       ) do
+    Endpoint.broadcast(@survey_results_topic, "rating_created", %{})
+
+    socket
+    |> put_flash(:info, "Rating submitted successfully")
+    |> assign(
+      :products,
+      List.replace_at(products, product_index, updated_product)
+    )
   end
 end
